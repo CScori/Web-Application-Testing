52763f7e18e3af58ca466f9b7da4a8fd70b73fbc
CScori <amilanotte1366@gmail.com>
2019-10-03 15:40:01 -0500

    adds mvp counts

--- stats-test/src/App.js
+++ stats-test/src/App.js
@@ -1,26 +1,31 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
-//onClick={() => set()}
+//onClick={() => set}
 
 function App() {
-const [balls, setBalls]= useState(0) //reset after 4  -> Set to zero after a hit
-const [strikes, setStrikes] = useState(0) //reset after 3  -> Set to zero after a hit
-const [hit, setHit] = useState()
-const [foul, setFoul] = useState() // increases strike by one -> after 2 fouls dont add to Strike count
+  const [balls, setBalls] = useState(0) //reset after 4  -> Set to zero after a hit
+  const [strikes, setStrikes] = useState(0) //reset after 3  -> Set to zero after a hit
+  const [hit, setHit] = useState(0)
+  const [foul, setFoul] = useState(1) // increases strike by one -> after 2 fouls dont add to Strike count
+  const [batter, setBatter] = useState()
 
   return (
     <div className="App">
+
       <section className="batter">
+
         <div className="display">
-          <div className="balls">Balls: {balls}</div>
           <div className="strikes">Strikes: {strikes}</div>
+          <div className="balls">Balls: {balls}</div>
+          <div className="hits">Hits: {hit}</div>
         </div>
 
         <div className="buttons">
-          <button  >Strike</button>
-          <button  >Ball</button>
-          <button  >Foul</button>
-          <button  >Hit</button>
+          <button onClick={() => setStrikes(strikes + 1 <= 3)} >Strike</button>
+          <button onClick={() => setBalls(balls + 1 <= 4)}>Ball</button>
+          <button onClick={() => setFoul(strikes + 1 <= 2)}>Foul</button>
+          <button onClick={() => setHit(setBalls(0), setStrikes(0))}>Hit</button>
+          <button onClick={() => setBatter(setBalls(0), setStrikes(0), setFoul(0))}>New Batter</button>
         </div>
       </section>
     </div>
