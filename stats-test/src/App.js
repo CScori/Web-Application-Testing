abdd951d34528c2476e8e2e7aedf1a688efad24e
CScori <amilanotte1366@gmail.com>
2019-10-03 14:36:12 -0500

    adds state logic setup for adding

--- stats-test/src/App.js
+++ stats-test/src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 //onClick={() => set()}
+
 function App() {
+const [balls, setBalls]= useState(0) //reset after 4  -> Set to zero after a hit
+const [strikes, setStrikes] = useState(0) //reset after 3  -> Set to zero after a hit
+const [hit, setHit] = useState()
+const [foul, setFoul] = useState() // increases strike by one -> after 2 fouls dont add to Strike count
+
   return (
     <div className="App">
       <section className="batter">
         <div className="display">
-          <div className="balls">Balls: 0</div>
-          <div className="strikes">Strikes: 0</div>
+          <div className="balls">Balls: {balls}</div>
+          <div className="strikes">Strikes: {strikes}</div>
         </div>
 
         <div className="buttons">
