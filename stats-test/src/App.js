9d79e7b7f80f9d77d2fdbd19829a1232ba0de6c3
CScori <amilanotte1366@gmail.com>
2019-10-03 22:04:44 -0500

    adds ball logic

--- stats-test/src/App.js
+++ stats-test/src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+const ballCount = (balls) => {
+  if (balls < 3) {
+    return balls + 1;
+  } else {
+    return 0
+  }
+}
+
 class App extends React.Component{
   constructor() {
     super();
